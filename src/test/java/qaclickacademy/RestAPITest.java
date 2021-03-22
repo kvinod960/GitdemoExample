c7788e746abd7eca86cdcce3edd51d55f93038eb
vinod <kvinod960@gmail.com>
2021-03-22 00:01:57 -0400

    This is commit from American guy

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -10,6 +10,10 @@ public class RestAPITest {
 		System.out.println("postJira");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
 	}
 	
 	@Test
